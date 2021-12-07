4c220a731c593008d80d6106d91ab55eba13c2cb
mgadegreef <94172285+mgadegreef@users.noreply.github.com>
2021-12-07 14:48:25 +0100

    Add files via upload

---
+++ 13_Opdracht__Run_a_JS_program_with_node/script.js
@@ -0,0 +1,22 @@
+// Voorbereiding
+console.log('Hello Winc Adademy Students');
+
+/*
+// Oefening 1
+// This is a large sum
+console.log(1230914 * 1823795);
+
+// Oefening 2
+console.log(637263 / 54);
+
+*/
+
+// Oefening 3
+
+// Oefening 4
+// Je ziet de comment bij oefening 1 niet, omdat deze uitgesloten wordt bij de uitvoering van de code.
+
+// Oefening 5
+
+// Oefening 6
+// Je ziet de berekeningen niet. Ze staan in een comment block en daarom worden deze uitgesloten bij de uitvoering van de code.
