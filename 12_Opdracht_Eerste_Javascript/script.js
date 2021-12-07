0f97cf40b6d09d0a8b098127f067af3c685eb8d6
mgadegreef <94172285+mgadegreef@users.noreply.github.com>
2021-12-07 11:43:49 +0100

    Add files via upload
    Eerste JavaScript pagina

---
+++ 12_Opdracht_Eerste_Javascript/script.js
@@ -0,0 +1,40 @@
+// oefening 1
+// oefening 2
+// oefening 3
+// oefening 4
+console.log('Hello Winc Academy');
+
+// oefening 5
+let naam = 'Martin';
+console.log(naam);
+
+// oefening 6
+// let if = 12; // niet mogelijk omdat 'if' een gereserveerd keyword is
+
+// oefening 7
+let numberA = '4';
+let numberB = '4';
+
+let numberC = 4;
+let numberD = 4;
+
+let ResultA = numberA + numberB; // is mogelijk, uikomst is 2 strings aan elkaar geplakt
+let ResultB = numberC + numberD; // is mogelijk, uitkomst is 2 numbers opgeteld
+
+console.log(ResultA); //
+console.log(ResultB);
+
+// oefening 8
+console.log(6 + 4);
+console.log(6 - 4);
+console.log(6 * 4);
+console.log(6 / 4);
+console.log(99 % 10);
+
+// oefening 9
+let age = 38;
+console.log(typeof age); 
+
+// oefening 10
+let otherAge = '38';
+console.log(typeof age); // interessant: type is ook een number, waar je een string verwacht
