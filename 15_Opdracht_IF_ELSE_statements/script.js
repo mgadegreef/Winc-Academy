4fc402b02e639523f8c68e72bce55f489023af9f
mgadegreef <94172285+mgadegreef@users.noreply.github.com>
2021-12-08 11:19:48 +0100

    Add files via upload

---
+++ 15_Opdracht_IF_ELSE_statements/script.js
@@ -0,0 +1,19 @@
+const age = 19;
+const isFemale = true;
+const driverStatus = "bob";
+
+if (isFemale == true){
+    if (age >= 18){
+        console.log("You are Female and 18 or above. Come on in and enjoy");
+    } else {
+        console.log("You are Female but to young. You cannot enter");
+    }}
+else{
+    console.log("You are not Female. This is Girls night, get out!");
+}
+
+if (driverStatus == "bob"){
+    console.log("You are allowed to drive");
+} else {
+    console.log("You are not allowed to drive");
+}
