3335b4643d3885fef10791d7bd1c815783b7066c
mgadegreef <94172285+mgadegreef@users.noreply.github.com>
2021-12-10 11:28:31 +0100

    Add files via upload

---
+++ Oefeningen_Function_basics/script.js
@@ -0,0 +1,27 @@
+const makePizza = function () {
+    console.log('- Deeg kneden en uitrollen');
+    console.log('- Tomatensaus op de pizza doen');
+    console.log('- Beleg de pizza met wat je wilt');
+    console.log('- Kaas erover strooien');
+    console.log('- 15 min in de oven op 200 graden');
+};
+
+const makeSushi = function (){
+    console.log('- Dun laagje sushirijst op het matje uitspreiden');
+    console.log('- plakjes zeewier erop leggen');
+    console.log('- stukjes zalm of andere vis in een strook erop leggen');
+    console.log('- rol met het matje tot een rolletje ');
+    console.log('- snij de rol in kleine stukjes en serveer met sojasaus en wasabi on the side');
+};
+
+// Nu zie je nog niets gebeuren, want we moeten de functie nog oproepen.
+
+
+// Oproepen functies
+
+makeSushi ();
+makeSushi ();
+makeSushi ();
+
+makePizza ();
+makePizza ();
