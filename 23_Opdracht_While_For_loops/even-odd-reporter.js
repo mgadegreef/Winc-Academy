3abe685976e283f207511699baf3c5379eca7447
mgadegreef <94172285+mgadegreef@users.noreply.github.com>
2021-12-14 14:55:56 +0100

    Add files via upload

---
+++ 23_Opdracht_While_For_loops/even-odd-reporter.js
@@ -0,0 +1,8 @@
+for (let i = 0; i <= 20; i++) {
+    console.log(`Is ${i} even or odd?`);
+    if (i % 2 === 0) {
+        console.log(`${i} is even`);
+    } else {
+        console.log(`${i} is odd`);
+    }
+}
---
+++ 23_Opdracht_While_For_loops/multiplication-tables.js
@@ -0,0 +1,11 @@
+for (i=1; i <= 10; i++) {
+    console.log(`${i} x 9 = ` + (i*9));
+}
+
+// Bonus
+
+for (x=1; x<= 10; x++){
+    for (i=1; i <= 10; i++) {
+        console.log(`${x} x ${i} = ` + (x*i));
+    }
+}
